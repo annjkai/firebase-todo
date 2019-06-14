cdbbe8c8c6e572261a876d0abdaae2590bccbcde
annika <ironannebonnie@gmail.com>
2019-06-14 12:10:11 +0200

    firebase init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: firebaseApiKey,
+    authDomain: firebaseAuthDomain,
+    databaseURL: firebaseDatabaseUrl,
+    projectId: firebaseProjectId,
+    storageBucket: firebaseStorageBucket,
+    messagingSenderId: firebaseMessagingSenderId,
+    appId: firebaseAppId
+  }
 };
 
 /*
